9afacea7935960fdd1c98a1e821707a4aeb25c7e
Anamaria <anamariaelek@gmail.com>
2024-08-20 17:58:32 +0200

    Bottom-up per-cell type motif hits reduce approach

---
+++ 09_Motif_syntax_bottom_up.R
@@ -0,0 +1,187 @@
+# log time
+message("# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # ")
+message(
+  "#                        ",
+  Sys.time(),
+  "                        #"
+)
+message("# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # ")
+
+# load packages
+library(GenomicRanges)
+library(data.table)
+library(tidyr)
+library(stringr)
+library(ggplot2)
+library(patchwork)
+library(ggrepel)
+source("motif-analysis/mta_downstream_functions.R")
+
+# set ggplot2 theme
+theme_py <- theme_light() + theme(
+  panel.grid.major = element_blank(),
+  panel.grid.minor = element_blank(),
+  panel.border = element_rect(colour = "black", fill = NA),
+  text = element_text(size=20),
+  strip.placement = "outside", 
+  strip.text = element_text(size=20, color="black"),
+  strip.background = element_rect(fill="white")
+)
+theme_set(theme_py)
+
+# directories
+adult_dir <- "ArchRProj_Nvec_TSS4_frag200"
+gastr_dir <- "ArchRProj_Nvec_gastrula"
+ann_dir <- "annotation"
+pks_dir <- "Results/Peaks"
+mta_dir <- "Results/Motifs"
+arc_dir <- "Results/Archetypes"
+syn_dir <- "Results/Syntax"
+dir.create(syn_dir, showWarnings = FALSE)
+fig_dir <- "Plots/Syntax"
+dir.create(fig_dir, showWarnings = FALSE)
+
+# cell types
+ct_cols <- c(
+  "cnidocyte"                  = "#ff42ff",
+  "cnidocyte_gastrula"         = "#f7abf7",
+  "ecto_pharynx"               = "#5bc0e8",
+  "ectoderm"                   = "#51a0be",
+  "ecto_aboral"                = "#045170",
+  "EMS"                        = "#bdf5bd",
+  "EMS_ecto_boundary"          = "#93dbce",
+  "gastro_circular_muscle_1"   = "#85c90e",
+  "gastro_circular_muscle_2"   = "#73b009",
+  "gastro_parietal_muscle"     = "#8ceb10",
+  "gastro_IRF1_2"              = "#c1eb05",
+  "gastro_somatic_gonad"       = "#bde314",
+  "muscle_tentacle_retractor"  = "#ffd700",
+  "muscle_mesentery_retractor" = "#f0e229",
+  "digestive_filaments_1"      = "#e33d3d",
+  "digestive_filaments_2"      = "#d10606",
+  "digestive_filaments_3"      = "#ad0303",
+  "epidermis_1"                = "#04ccd4",
+  "epidermis_2"                = "#16bacc",
+  "precursors_PGC"             = "#bebebe",
+  "precursors_endoNPC"         = "#8a8686",
+  "precursors_NPC"             = "#636363",
+  "NPC_1"                      = "#808d91",
+  "NPC_2"                      = "#758d92",
+  "neuron_GATA_Islet_1"        = "#0c82f7",
+  "neuron_GATA_Islet_2"        = "#1175f0",
+  "neuron_Pou4_FoxL2_1"        = "#101cde",
+  "neuron_Pou4_FoxL2_2"        = "#0b16bf",
+  "neuron_Pou4_FoxL2_3"        = "#2e39dd",
+  "neuronal_gastrula"          = "#063cb9",
+  "gland"                      = "#ff6f08",
+  "gland_mucin"                = "#ff8f12"
+)
+cell_types <- names(ct_cols)
+adult_cell_types <- c(
+  "cnidocyte",
+  "gastro_circular_muscle_1", 
+  "gastro_circular_muscle_2",
+  "gastro_parietal_muscle",
+  "gastro_IRF1_2",
+  "gastro_somatic_gonad",
+  "muscle_mesentery_retractor",
+  "muscle_tentacle_retractor",
+  "digestive_filaments_1",
+  "digestive_filaments_2",
+  "digestive_filaments_3",
+  "epidermis_1",
+  "epidermis_2",
+  "precursors_PGC",
+  "precursors_endoNPC",
+  "precursors_NPC",
+  "neuron_GATA_Islet_1",
+  "neuron_GATA_Islet_2",
+  "neuron_Pou4_FoxL2_1",
+  "neuron_Pou4_FoxL2_2",
+  "neuron_Pou4_FoxL2_3",
+  "gland"
+)
+gastr_cell_types <- c(setdiff(cell_types, adult_cell_types))
+
+# cell type is the first argument passed to this script
+ct <- as.character(commandArgs(trailingOnly = TRUE)[1])
+
+# overlap fraction is the second argument passed to this script
+reciprocal_overlap <- as.numeric(commandArgs(trailingOnly = TRUE)[2])
+
+# load motif scores
+message("Loading motif scores")
+arc_id <- "PPM-PCC-0.8-IC0.5-5bp"
+q <- 0.95
+mta_dt <- rbindlist(lapply(c(
+  # archetypes
+  file.path(arc_dir, sprintf("motif-scores-archetypes-%s-mona-q%s.tsv.gz", arc_id, q)),
+  # experimental motifs
+  file.path(mta_dir, sprintf("motif-scores-mona-q%s.tsv.gz", q))
+), fread))
+
+# calculate relative motif scores (independent of the motif length)
+mta_dt[, relative_motif_score := motif_score / max(motif_score)]
+
+# subset enriched motifs
+mta_en <- fread(file.path(mta_dir, "motif-enrichment-mona-q0.98-FC-1-padj-0.001.tsv"))
+mta_en <- unique(mta_en[,.(cell_type, archetype_name, fc, pval, padj)])
+setnames(mta_en, "archetype_name", "motif")
+mta_dt <- mta_dt[motif %in% mta_en$motif]
+
+# all peaks
+peaks <- fread(file.path(pks_dir, "Peaks_cell_type_mapped.bed"))
+setnames(peaks, c("seqnames", "start", "end", "peak", "score", "strand"))
+peaks <- unique(peaks)
+peaks_gr <- makeGRangesFromDataFrame(peaks, keep.extra.columns = TRUE)
+
+# cell type peaks
+message(sprintf("%s | Reducingp peaks for %s", Sys.time(), ct))
+stg_dir <- ifelse(ct %in% adult_cell_types, adult_dir, gastr_dir)
+pks_fn <- file.path(
+  stg_dir, "ArchRProj", "PeakDifferential", "cell_type_filtered",
+  sprintf("Peaks-%s-vs-others.tsv", ct)
+)
+pks_dt <- fread(pks_fn, select = 1:3)
+setnames(pks_dt, c("seqnames", "start", "end"))
+
+pks_gr <- makeGRangesFromDataFrame(pks_dt)
+
+# overlap peaks (to map per-stage peak ids to final peak set ids)
+pks_ovl <- findOverlaps(query = peaks_gr, subject = pks_gr)
+pks_dat <- peaks_gr[queryHits(pks_ovl)]
+
+# peaks in cell type
+mta_ct_dt <- mta_dt[peak %in% pks_dat$peak]
+
+# motif enrichment in cell type
+mta_ct_en <- mta_en[cell_type == ct]
+
+# combine
+mta_ct <- merge.data.table(mta_ct_dt, mta_ct_en, by = "motif", all.x = TRUE)
+
+# make ranges
+mta_gr <- makeGRangesFromDataFrame(mta_ct, keep.extra.columns = TRUE)
+
+# reduce motif hits
+red_res <- mta_reduce_motif_hits(
+  mta_gr, 
+  reciprocal_overlap = reciprocal_overlap,
+  order_col = "fc",
+  order_decrease = TRUE
+)
+message(sprintf("%s | Done reducing hits for %s", Sys.time(), ct))
+
+# save results
+out_fn <- file.path(syn_dir, sprintf("motif-hits-reduced-%s.rds", ct))
+saveRDS(red_res, out_fn)
+message("Saved to: ", out_fn)
+
+# log time
+message("# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # ")
+message(
+  "#                        ",
+  Sys.time(),
+  "                        #"
+)
+message("# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # ")
